2432c1f67b4dba03529faec2866a5ca3afaba3fe
Mario-D93 <mariodudas93@gmail.com>
2020-01-03 17:34:25 +0100

    styling visual attributes

--- time_graph.py
+++ time_graph.py
@@ -1,5 +1,14 @@
 from main import data_frame
 from bokeh.plotting import  show, output_file, figure
+from bokeh.models import HoverTool, ColumnDataSource
+
+#converting datetime types of the frame to string
+data_frame["Start_str"]=data_frame["Start"].dt.strftime("%Y-%m-%d %H:%M:%S")
+data_frame["End_str"]=data_frame["End"].dt.strftime("%Y-%m-%d %H:%M:%S")
+
+
+#converting datas to the ColumnDataSource object to enable bokeh_plotting to acces them
+cds=ColumnDataSource(data_frame)
 
 plot=figure(x_axis_type='datetime', height=300, width=1000, title='Motion_Detector Graph')
 
@@ -10,7 +19,10 @@ plot.outline_line_color = "navy"
 plot.yaxis.minor_tick_line_color=None
 plot.ygrid[0].ticker.desired_num_ticks=1
 
-quad=plot.quad(bottom=0, top=1, left=data_frame["Start"], right=data_frame["End"], color="grey")
+quad=plot.quad(bottom=0, top=1, left="Start", right="End", color="grey", source=cds)
+
+hover=HoverTool(tooltips=[("Start","@Start_str"),("End","@End_str")])
+plot.add_tools(hover)
 
 output_file("motion_graph.html")
 show(plot)
